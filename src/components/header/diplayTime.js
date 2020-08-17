125658edfec757276245376ba876c24fffa37b0a
Pierre Vanobbergen <pierrevob@hotmail.com>
2020-08-17 16:00:07 +0200

    Ajout Pierre, Child-Parent and State

--- src/components/header/diplayTime.js
+++ src/components/header/diplayTime.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 class DisplayTime extends React.Component {
 
+    state = {
+        dateCourante : new Date()
+    };
+
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({
+                dateCourante : new Date()
+            })
+        }, 1000)
+    }
+
     render() {
-        let dateCourante = new Date();
-        let tempsJavaScript = dateCourante.getTime();
+        let tempsJavaScript = this.state.dateCourante.getTime();
         let tempsPHP = tempsJavaScript/1000;
 
         return (
@@ -21,7 +32,7 @@ class DisplayTime extends React.Component {
                 }
                 { this.props.format === "humanTime" &&
                      <p>
-                        Date pour un humain: {dateCourante.getDate()}/{dateCourante.getMonth()}/{dateCourante.getFullYear()}
+                        Date pour un humain: {this.state.dateCourante.getDate()}/{this.state.dateCourante.getMonth()}/{this.state.dateCourante.getFullYear()}
                     </p>
                 }
             </div>
